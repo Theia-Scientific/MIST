c18021e831b1146336ab0795635e8430809141ba
Christopher Field <chris.field@theiascientific.com>
2025-02-01 15:44:14 -0500

    Add tests

---
+++ tests/test_cli.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import importlib.metadata
+
+from tstiler import __app_name__
+from tstiler.cli import app, main, map_verbosity
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_map_verbosity_false():
+    actual = map_verbosity(False)
+    assert actual == "INFO"
+
+
+def test_map_verbosity_true():
+    actual = map_verbosity(True)
+    assert actual == "DEBUG"
+
+
+def test_main():
+    main(verbose=False, version=False)
+
+
+def test_app_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_app_version():
+    version = importlib.metadata.version(__app_name__)
+    result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{__app_name__} {version}" in result.stdout
