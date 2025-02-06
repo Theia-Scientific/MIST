2d0096ba106dbda898ed857a9c9b86863d99c93d
Christopher Field <cfield2@gmail.com>
2025-02-06 08:47:34 -0800

    Removed global result generation for fewer iterations (#9)
    * Fix no detections taking forever
    
    * Change commented code
    
    * Remove extraneous array manipulation
    
    * Add global XY efficiently
    
    * Add new type
    
    * Remove unused types and functions
    
    * Remove unnecessary array creation
    
    ---------
    
    Co-authored-by: Christopher Field <chris.field@theiascientific.com>

--- tstiler/cli.py
+++ tstiler/cli.py
@@ -88,11 +88,10 @@ class Tile(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
-class TileResult(BaseModel):
-    boxes: List[List[int]]
-    class_indices: List[int]
-    masks: np.ndarray
-    scores: np.ndarray
+class TileMask(BaseModel):
+    data: np.ndarray
+    offset_x: int
+    offset_y: int
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -106,27 +105,11 @@ class TileVisual(BaseModel):
     y_max: int
 
 
-class GlobalResult(BaseModel):
-    boxes: List[List[int]] = []
-    masks: List[np.ndarray] = []
-
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-
 class Instance(BaseModel):
     box: List[int]
     class_index: int
     id: int
     mask: np.ndarray
-    scores: List[float]
-
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-
-class CombineResult(BaseModel):
-    boxes: List[List[int]]
-    class_indices: List[int]
-    masks: List[np.ndarray]
     scores: List[float]
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -273,69 +256,6 @@ def create_patched_tiles(
         plt.show()
     LOGGER.info(f"Number of generated tiles: {count}")
     return tiles
-
-
-def resize_result(
-    global_result: GlobalResult,
-    original_image_size: Tuple[int, int],
-    resized_image_size: Tuple[int, int],
-) -> GlobalResult:
-    LOGGER.info("Resizing global result...")
-    original_width, original_height = original_image_size
-    resized_width, resized_height = resized_image_size
-    resized_xyxy = []
-    resized_masks = []
-
-    for bbox in global_result.boxes:
-        x_min, y_min, x_max, y_max = bbox
-        x_min_resized = int(x_min * (original_width / resized_width))
-        y_min_resized = int(y_min * (original_height / resized_height))
-        x_max_resized = int(x_max * (original_width / resized_width))
-        y_max_resized = int(y_max * (original_height / resized_height))
-        resized_xyxy.append(
-            [x_min_resized, y_min_resized, x_max_resized, y_max_resized]
-        )
-    for mask in global_result.masks:
-        mask_resized = cv2.resize(
-            mask,
-            (original_width, original_height),
-            interpolation=cv2.INTER_NEAREST,
-        )
-        resized_masks.append(mask_resized.astype(np.uint8))
-    LOGGER.info("Resizing global result...DONE")
-    return GlobalResult(boxes=resized_xyxy, masks=resized_masks)
-
-
-def calculate_global_result(
-    tile: Tile, tile_result: TileResult, src_image_size: Tuple[int, int]
-) -> GlobalResult:
-    global_result = GlobalResult()
-    global_x_start = tile.x_start
-    global_y_start = tile.y_start
-    global_width, global_height = src_image_size
-    tile_height, tile_width, *_ = tile.img.shape
-    for bbox in tile_result.boxes:
-        tile_x_min, tile_y_min, tile_x_max, tile_y_max = bbox
-        global_x_min = tile_x_min + global_x_start
-        global_y_min = tile_y_min + global_y_start
-        global_x_max = tile_x_max + global_x_start
-        global_y_max = tile_y_max + global_y_start
-        global_result.boxes.append(
-            [global_x_min, global_y_min, global_x_max, global_y_max]
-        )
-    for mask in tile_result.masks:
-        black_image = np.zeros((global_height, global_width))
-        mask_resized = cv2.resize(
-            np.array(mask).copy(),
-            (tile_width, tile_height),
-            interpolation=cv2.INTER_NEAREST,
-        )
-        black_image[
-            global_y_start : global_y_start + tile_height,
-            global_x_start : global_x_start + tile_width,
-        ] = mask_resized
-        global_result.masks.append(black_image.astype(np.uint8))
-    return global_result
 
 
 def calculate_mask_iou(mask: np.ndarray, masks: List[np.ndarray]) -> torch.Tensor:
@@ -549,12 +469,14 @@ def combine_with_nms(
 
 def combine(
     class_indices: List[int],
-    masks: List[np.ndarray],
+    masks: List[TileMask],
     src_image_size: Tuple[int, int],
+    tile_size: Tuple[int, int],
     dump_masks: bool = False,
     merge_classes: List[int] = [],
 ) -> List[Instance]:
     LOGGER.info("Combining...")
+    tile_width, tile_height = tile_size
     src_image_width, src_image_height = src_image_size
     tensor_class_indices = torch.tensor(class_indices)
     instance_id = 0
@@ -570,14 +492,18 @@ def combine(
             class_masks = [masks[i] for i in cls_indexes]
             LOGGER.debug(f"class masks count = {len(class_masks)}")
             class_mask = np.zeros((src_image_height, src_image_width))
+            LOGGER.debug(f"class_mask.shape = {class_mask.shape}")
             for i, mask in enumerate(class_masks):
-                class_mask = np.logical_or(class_mask, mask)
+                class_mask[
+                    mask.offset_y : mask.offset_y + tile_height,
+                    mask.offset_x : mask.offset_x + tile_width,
+                ] += mask.data
                 if dump_masks:
                     cv2.imwrite(
                         f"tmp/{cls_index}/{i}c.png", class_mask.astype(np.uint8) * 255
                     )
                     cv2.imwrite(
-                        f"tmp/{cls_index}/{i}m.png", mask.astype(np.uint8) * 255
+                        f"tmp/{cls_index}/{i}m.png", mask.data.astype(np.uint8) * 255
                     )
             class_mask = class_mask.astype(np.uint8)
             contours, _ = cv2.findContours(
@@ -819,8 +745,6 @@ def main(
                 tile_size=(tile_width, tile_height),
                 overlap=(overlap_width, overlap_height),
             )
-            confidences = []
-            boxes = []
             masks = []
             class_indices = []
             visual_tiles = []
@@ -839,25 +763,20 @@ def main(
                     verbose=not inference_silent,
                 )
                 pred = results[0]
+                tile_class_indices = pred.boxes.cls.cpu().int().tolist()
+                class_indices.extend(tile_class_indices)
                 if pred.masks is None:
-                    masks_data = np.zeros(tile.img.shape)
+                    masks_data = np.zeros(
+                        (len(tile_class_indices), tile_height, tile_width)
+                    )
                 else:
                     masks_data = pred.masks.data.cpu().numpy().astype(np.uint8)
-                tile_boxes = pred.boxes.xyxy.cpu().int().tolist()
-                tile_class_indices = pred.boxes.cls.cpu().int().tolist()
-                tile_masks = masks_data
-                tile_scores = pred.boxes.conf.cpu().numpy()
-                tile_result = TileResult(
-                    boxes=tile_boxes,
-                    class_indices=tile_class_indices,
-                    masks=tile_masks,
-                    scores=tile_scores,
-                )
-                global_result = calculate_global_result(tile, tile_result, orig_size)
-                confidences.extend(tile_result.scores)
-                boxes.extend(global_result.boxes)
-                masks.extend(global_result.masks)
-                class_indices.extend(tile_result.class_indices)
+                for data in masks_data:
+                    masks.append(
+                        TileMask(
+                            data=data, offset_x=tile.x_start, offset_y=tile.y_start
+                        )
+                    )
                 if show_tiles:
                     visual_tiles.append(
                         TileVisual(
@@ -871,6 +790,7 @@ def main(
                 class_indices,
                 masks,
                 orig_size,
+                (tile_width, tile_height),
                 dump_masks=dump_masks,
                 merge_classes=merge_classes,
             )
