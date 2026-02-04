d8db1ac3cf01f258da175c1f71816d9b9dc33a1b
Christopher Field <chris.field@theiascientific.com>
2026-02-04 14:30:38 -0500

    Remove monkeypatch usage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -164,14 +164,13 @@ def test_main_zip(zip_file, weights_file):
     main(weights_file, [zip_file], verbose=False, version=False)
 
 
-def test_create_tiles_show(mocker, monkeypatch, blank_image):
-
+def test_create_tiles_show(mocker, blank_image):
     def mock_figure(*args, **kwargs):
         _ = args
         _ = kwargs
         return mocker.MagicMock(spec=Figure)
 
-    monkeypatch.setattr("matplotlib.pyplot.figure", mock_figure)
+    mocker.patch("matplotlib.pyplot.figure", mock_figure)
     create_tiles(blank_image)
 
 
