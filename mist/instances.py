2f384ad11c8aaba6944c87af23605bd2b3b7faac
Christopher Field <chris.field@theiascientific.com>
2026-02-06 11:12:48 -0500

    Add instances module

---
+++ mist/instances.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+class Instance(BaseModel):
+    box: List[int]
+    class_index: int
+    id: int
+    mask: np.ndarray
+    scores: List[float]
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
