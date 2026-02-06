7d7a3b38c8c0bff3449c6f31659f4be6496fc3b4
Christopher Field <chris.field@theiascientific.com>
2026-02-06 11:32:17 -0500

    Add utils module

--- mist/cli.py
+++ mist/cli.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-import cv2
 import importlib.metadata
-import io
 import json
 import logging
-import mimetypes
 import numpy as np
-import tifffile
 import typer
 import zipfile
 
@@ -16,6 +12,7 @@ from pathlib import Path
 from mist import __app_name__
 from mist.merging import merge
 from mist.tiling import create_tiles, TileMask, TileVisual
+from mist.utils import read_image_file
 from mist.visualizing import visualize
 from typing import List, Optional
 from ultralytics.models import YOLO
@@ -24,22 +21,9 @@ logging.getLogger("matplotlib.font_manager").disabled = True
 
 LOGGER: logging.Logger = logging.getLogger(__name__)
 
-BIT_DEPTH_DTYPE: str = "uint8"
-COLOR_CHANNEL_COUNT: int = 3
-NPY_MIME_TYPE: str = "application/numpy"
 PREFIX: str = f"{__app_name__.upper()}"
-TIFF_MIME_TYPE: str = "image/tiff"
 
 app = typer.Typer(pretty_exceptions_show_locals=False)
-
-
-class UnknownMimeTypeError(Exception):
-    def __init__(self, file_name: str):
-        self.file_name = file_name
-
-
-class ImageDecodeError(Exception):
-    pass
 
 
 def map_verbosity(enabled: bool) -> str:
@@ -54,51 +38,6 @@ def version_callback(value: bool):
         version = importlib.metadata.version(__app_name__)
         print(f"{__app_name__} {version}")
         raise typer.Exit()
-
-
-def count_channels(img: np.ndarray) -> int:
-    return img.shape[-1] if img.ndim == 3 else 1
-
-
-def correct_cv_image(src: np.ndarray) -> np.ndarray:
-    if src.dtype == BIT_DEPTH_DTYPE:
-        corrected_image = src
-    else:
-        src_max = np.max(src)
-        src_min = np.min(src)
-        if src_max == src_min:
-            normalized_image = src.astype(np.float32)
-        else:
-            normalized_image = ((src - src_min) / (src_max - src_min)).astype(
-                np.float32
-            )
-        corrected_image = np.round(normalized_image * 256).astype(BIT_DEPTH_DTYPE)
-    if count_channels(corrected_image) < COLOR_CHANNEL_COUNT:
-        three_channel_image = cv2.cvtColor(corrected_image, cv2.COLOR_GRAY2BGR)
-    else:
-        three_channel_image = corrected_image
-    return three_channel_image
-
-
-def decode_data(data: bytes, mime_type: str) -> np.ndarray:
-    if mime_type == NPY_MIME_TYPE:
-        return np.load(io.BytesIO(data), allow_pickle=True)
-    elif mime_type == TIFF_MIME_TYPE:
-        return correct_cv_image(tifffile.imread(io.BytesIO(data)))
-    else:
-        src = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_UNCHANGED)
-        if src is None:
-            raise ImageDecodeError()
-        return correct_cv_image(src)
-
-
-def read_image_file(source: Path) -> np.ndarray:
-    with open(source, "rb+") as f:
-        data = f.read()
-    mime_type = mimetypes.guess_type(source)[0]
-    if mime_type is None:
-        raise UnknownMimeTypeError(source.name)
-    return decode_data(data, mime_type)
 
 
 @app.command()
@@ -183,7 +122,9 @@ def main(
         elif zipfile.is_zipfile(src):
             pass
         else:
+            LOGGER.info("Reading image file...")
             original_img = read_image_file(src)
+            LOGGER.info("Reading image file...DONE")
             orig_height, orig_width, *_ = original_img.shape
             orig_size = (orig_width, orig_height)
             LOGGER.info("Creating tiles...")
