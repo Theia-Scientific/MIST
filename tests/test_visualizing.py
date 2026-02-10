4b01669ff85406e4fea83784dbafc2447b387c93
Christopher Field <chris.field@theiascientific.com>
2026-02-10 12:40:11 -0500

    Add test

--- tests/test_visualizing.py
+++ tests/test_visualizing.py
@@ -40,3 +40,16 @@ def test_run_with_show_classes_list(mock_plt_show, bus_jpg, weights_file):
         show_classes_list=[0]
     )
     assert labeled_image.any()
+
+
+def test_run_with_no_random_colors(mock_plt_show, bus_jpg, weights_file):
+    _ = mock_plt_show
+    result = detecting.run(bus_jpg, YOLO(weights_file), "cpu")
+    labeled_image = visualizing.run(
+        result.instances,
+        result.original_image,
+        result.class_names,
+        random_object_colors=False,
+        list_of_class_colors=None
+    )
+    assert labeled_image.any()
