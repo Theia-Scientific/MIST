1867597c3764d39e72f854cfe34ed8698922059b
Christopher Field <chris.field@theiascientific.com>
2026-02-10 12:34:52 -0500

    Add tests

--- tests/test_visualizing.py
+++ tests/test_visualizing.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import pytest
+
 from mist import detecting, visualizing
 from ultralytics.models import YOLO
 
-def test_run(mocker, bus_jpg, weights_file):
+@pytest.fixture
+def mock_plt_show(mocker):
     def mock_plt_show(*args, **kwargs):
         _ = args
         _ = kwargs
@@ -11,6 +14,29 @@ def test_run(mocker, bus_jpg, weights_file):
         return None
 
     mocker.patch("matplotlib.pyplot.show", mock_plt_show)
+   
+
+def test_run(mock_plt_show, bus_jpg, weights_file):
+    _ = mock_plt_show
     result = detecting.run(bus_jpg, YOLO(weights_file), "cpu")
     labeled_image = visualizing.run(result.instances, result.original_image, result.class_names)
+    assert labeled_image.any()
+
+
+def test_run_with_no_class_names(mock_plt_show, bus_jpg, weights_file):
+    _ = mock_plt_show
+    result = detecting.run(bus_jpg, YOLO(weights_file), "cpu")
+    labeled_image = visualizing.run(result.instances, result.original_image, [])
+    assert labeled_image.any()
+
+
+def test_run_with_show_classes_list(mock_plt_show, bus_jpg, weights_file):
+    _ = mock_plt_show
+    result = detecting.run(bus_jpg, YOLO(weights_file), "cpu")
+    labeled_image = visualizing.run(
+        result.instances,
+        result.original_image,
+        result.class_names,
+        show_classes_list=[0]
+    )
     assert labeled_image.any()
