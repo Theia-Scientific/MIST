e7752c6809c1130bc9ab44643ad76a9b4e2a9cac
Christopher Field <chris.field@theiascientific.com>
2026-02-10 12:09:06 -0500

    Add tests

--- tests/test_detecting.py
+++ tests/test_detecting.py
@@ -4,8 +4,8 @@ from mist.detecting import run
 from ultralytics.models import YOLO
 
 def test_run(bus_jpg, weights_file):
-    run(bus_jpg, YOLO(weights_file))
+    run(bus_jpg, YOLO(weights_file), device="cpu")
 
 
 def test_run_with_no_predictions(blank_png, weights_file):
-    run(blank_png, YOLO(weights_file))
+    run(blank_png, YOLO(weights_file), device="cpu")
