7175c2342a6cf0d6e4525ad6ad9d6b3a59846015
Christopher Field <chris.field@theiascientific.com>
2026-02-12 15:39:16 -0500

    Fix lint warning

--- mist/detecting.py
+++ mist/detecting.py
@@ -137,8 +137,8 @@ def run(
         instances=instances,
         original_image=original_img,
         stats=Stats(
-            merged = Counter(instance_class_names),
-            unmerged = Counter(all_class_names)
+            merged=Counter(instance_class_names),
+            unmerged=Counter(all_class_names)
         ),
         visual_tiles=visual_tiles
     )
