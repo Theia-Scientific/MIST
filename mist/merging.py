939aff0fd7b2c9caba1433fdf70f1450aa7e98a0
Christopher Field <chris.field@theiascientific.com>
2026-02-12 15:40:31 -0500

    Fix lint warning

--- mist/merging.py
+++ mist/merging.py
@@ -60,8 +60,8 @@ def run(
             logger.debug(f"class_mask.shape = {class_mask.shape}")
             for i, mask in enumerate(class_masks):
                 class_mask[
-                    mask.offset_y : mask.offset_y + tile_height,
-                    mask.offset_x : mask.offset_x + tile_width,
+                    mask.offset_y:mask.offset_y + tile_height,
+                    mask.offset_x:mask.offset_x + tile_width,
                 ] += mask.data
                 if dump_masks:
                     dst = dump_masks_to.joinpath(str(cls_index_int))
