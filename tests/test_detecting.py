178068b99a0209d7c2077ff6be5d1e5954e72704
Christopher Field <chris.field@theiascientific.com>
2026-02-20 17:25:22 -0500

    Fix tests

--- tests/test_detecting.py
+++ tests/test_detecting.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 
 import os
+import pytest
 
 from mist.detecting import run
-from ultralytics.models import YOLO
+from mist.models import Inference, yolo
 
 
-def test_run(bus_jpg, weights_file):
-    result = run(bus_jpg, YOLO(weights_file), device="cpu")
+@pytest.fixture
+def model(weights_file) -> Inference:
+    return yolo.Model(weights_file, device="cpu")
+
+
+def test_run(bus_jpg, model):
+    result = run(bus_jpg, model)
     assert len(result.class_names) > 0
     assert len(result.instances) > 0
 
 
-def test_run_with_no_predictions(blank_png, weights_file):
-    result = run(blank_png, YOLO(weights_file), device="cpu")
+def test_run_with_no_predictions(blank_png, model):
+    result = run(blank_png, model)
     assert len(result.class_names) > 0
     assert len(result.instances) == 0
 
 
-def test_run_with_dump_masks(bus_jpg, tmp_path, weights_file):
+def test_run_with_dump_masks(bus_jpg, model, tmp_path):
     result = run(
         bus_jpg,
-        YOLO(weights_file),
-        device="cpu",
+        model,
         dump_masks=True,
-        dump_masks_to=tmp_path
+        dump_masks_to=tmp_path,
     )
     assert len(result.class_names) > 0
     assert len(result.instances) > 0
