afed108262c0f998fa4364eaab5eae7e3f208d68
Christopher Field <chris.field@theiascientific.com>
2026-09-16 15:22:36 -0400

    Fix tests

--- tests/test_detecting.py
+++ tests/test_detecting.py
@@ -25,35 +25,32 @@ def bus_image(bus_jpg: Path) -> npt.NDArray[np.uint8]:
 
 def test_run(bus_image: npt.NDArray[np.uint8], model: Model):
     predict, class_names = model
-    result = run(bus_image, predict, class_names)
-    assert len(result.class_names) > 0
-    assert len(result.instances) > 0
+    detections = run(bus_image, predict, class_names)
+    assert len(detections) > 0
 
 
 def test_run_with_no_predictions(blank_image: npt.NDArray[np.uint8], model: Model):
     predict, class_names = model
-    result = run(blank_image, predict, class_names)
-    assert len(result.class_names) > 0
-    assert len(result.instances) == 0
+    detections = run(blank_image, predict, class_names)
+    assert len(detections) > 0
 
 
 def test_run_with_erosion(bus_image: npt.NDArray[np.uint8], model: Model):
     predict, class_names = model
-    result = run(
+    detections = run(
         bus_image,
         predict,
         class_names,
         erosion=ErosionConfiguration(enabled=True),
     )
-    assert len(result.class_names) > 0
-    assert len(result.instances) > 0
+    assert len(detections) > 0
 
 
 def test_run_with_dump_masks(
     bus_image: npt.NDArray[np.uint8], model: Model, tmp_path: Path
 ):
     predict, class_names = model
-    result = run(
+    detections = run(
         bus_image,
         predict,
         class_names,
@@ -61,14 +58,11 @@ def test_run_with_dump_masks(
             clazz=True, data=True, erode=True, instance=True, to=tmp_path
         ),
     )
-    assert len(result.class_names) > 0
-    assert len(result.instances) > 0
+    assert len(detections) > 0
     assert len(os.listdir(tmp_path)) > 0
 
 
 def test_empty_detections(blank_image: npt.NDArray[np.uint8], empty_detections: Model):
     predict, class_names = empty_detections
-    result = run(blank_image, predict, class_names)
-
-    assert len(result.class_names) == 1
-    assert len(result.instances) == 0
+    detections = run(blank_image, predict, class_names)
+    assert len(detections) == 1
