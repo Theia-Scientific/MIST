790331c0c3285990a10485a92db1c79a06e422fc
Christopher Field <chris.field@theiascientific.com>
2026-09-16 15:41:06 -0400

    Fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -124,18 +124,31 @@ def test_app_version():
     assert f"{__app_name__} {version}" in result.stdout
 
 
-def test_app_image(blank_png: Path, weights_file: Path):
-    result = runner.invoke(app, ["--device=cpu", str(weights_file), str(blank_png)])
-    assert result.exit_code == 0
-
-
-def test_app_directory(dir_with_images: Path, weights_file: Path):
+def test_app_image(blank_png: Path, tmp_path: Path, weights_file: Path):
     result = runner.invoke(
-        app, ["--device=cpu", str(weights_file), str(dir_with_images)]
+        app,
+        ["--device=cpu", "--output", str(tmp_path), str(weights_file), str(blank_png)],
     )
     assert result.exit_code == 0
 
 
-def test_app_zip(zip_file: Path, weights_file: Path):
-    result = runner.invoke(app, ["--device=cpu", str(weights_file), str(zip_file)])
+def test_app_directory(dir_with_images: Path, tmp_path: Path, weights_file: Path):
+    result = runner.invoke(
+        app,
+        [
+            "--device=cpu",
+            "--output",
+            str(tmp_path),
+            str(weights_file),
+            str(dir_with_images),
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_app_zip(zip_file: Path, tmp_path: Path, weights_file: Path):
+    result = runner.invoke(
+        app,
+        ["--device=cpu", "--output", str(tmp_path), str(weights_file), str(zip_file)],
+    )
     assert result.exit_code == 0
